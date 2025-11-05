0682a192bcf4cdc990a32de49304f310fba33ec4
Hoyoung027 <hybyun0207@gmail.com>
2025-11-05 17:38:43 +0900

    init : initial setting

---
+++ src/main/java/ceos/diggindie/DiggindieApplication.java
@@ -0,0 +1,13 @@
+package ceos.diggindie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiggindieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiggindieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ceos/diggindie/DiggindieApplicationTests.java
@@ -0,0 +1,13 @@
+package ceos.diggindie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiggindieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
