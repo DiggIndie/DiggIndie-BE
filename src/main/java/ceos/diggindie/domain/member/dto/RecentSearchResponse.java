8dcc1ee225fda0ed2f08136b9ee02a2bbfbfa09e
Hoyoung927 <hybyun0207@gmail.com>
2026-01-12 00:33:27 +0900

    fix : 최근 검색어 응답 dto 수정

--- src/main/java/ceos/diggindie/domain/member/dto/RecentSearchResponse.java
+++ src/main/java/ceos/diggindie/domain/member/dto/RecentSearchResponse.java
@@ -13,14 +13,14 @@ public class RecentSearchResponse {
     @Getter
     @Builder
     public static class RecentSearchInfo {
-        private Long id;
+        private Long recentSearchId;
         private String content;
         private SearchCategory category;
         private LocalDateTime createdAt;
 
         public static RecentSearchInfo from(RecentSearch recentSearch) {
             return RecentSearchInfo.builder()
-                    .id(recentSearch.getId())
+                    .recentSearchId(recentSearch.getId())
                     .content(recentSearch.getContent())
                     .category(recentSearch.getCategory())
                     .createdAt(recentSearch.getCreatedAt())
