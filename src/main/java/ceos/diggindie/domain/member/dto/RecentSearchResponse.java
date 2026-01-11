a7b5ec197742b2b56789dedf442065193c1ff72e
Hoyoung927 <hybyun0207@gmail.com>
2026-01-12 00:15:42 +0900

    feat : 검색어 관리 API 작성

---
+++ src/main/java/ceos/diggindie/domain/member/dto/RecentSearchResponse.java
@@ -0,0 +1,45 @@
+package ceos.diggindie.domain.member.dto;
+
+import ceos.diggindie.common.enums.SearchCategory;
+import ceos.diggindie.domain.member.entity.RecentSearch;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class RecentSearchResponse {
+
+    @Getter
+    @Builder
+    public static class RecentSearchInfo {
+        private Long id;
+        private String content;
+        private SearchCategory category;
+        private LocalDateTime createdAt;
+
+        public static RecentSearchInfo from(RecentSearch recentSearch) {
+            return RecentSearchInfo.builder()
+                    .id(recentSearch.getId())
+                    .content(recentSearch.getContent())
+                    .category(recentSearch.getCategory())
+                    .createdAt(recentSearch.getCreatedAt())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class RecentSearchListDTO {
+        private List<RecentSearchInfo> searches;
+
+        public static RecentSearchListDTO from(List<RecentSearch> recentSearches) {
+            List<RecentSearchInfo> searchInfos = recentSearches.stream()
+                    .map(RecentSearchInfo::from)
+                    .toList();
+            return RecentSearchListDTO.builder()
+                    .searches(searchInfos)
+                    .build();
+        }
+    }
+}
