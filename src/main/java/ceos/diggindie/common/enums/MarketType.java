e5856ab96bc223f8ea1ab9237531917a3d862ad4
Yoonji Lee <estelle0329@ewha.ac.kr>
2026-01-16 13:59:08 +0900

    feat: enum 추가 (기획 변경)

--- src/main/java/ceos/diggindie/common/enums/MarketType.java
+++ src/main/java/ceos/diggindie/common/enums/MarketType.java
@@ -1,5 +1,34 @@
 package ceos.diggindie.common.enums;
 
+import ceos.diggindie.common.code.GeneralErrorCode;
+import ceos.diggindie.common.exception.GeneralException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MarketType {
-    GOODS, TICKET, ETC
+    SELL("sell", "판매"),
+    BUY("buy", "구매");
+
+    private final String value;
+    private final String description;
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static MarketType from(String value) {
+        for (MarketType type : MarketType.values()) {
+            if (type.value.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new GeneralException(GeneralErrorCode.BAD_REQUEST,
+                "지원하지 않는 거래 타입입니다: " + value + ". (sell, buy 중 선택해주세요.)");
+    }
 }
