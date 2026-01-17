27a5abd2ba836d920a5be409de29fbfef4db1406
Yoonji Lee <estelle0329@ewha.ac.kr>
2026-01-17 16:10:13 +0900

    fix: 아티스트 상세 조회 관련 문제 해결

--- src/main/java/ceos/diggindie/domain/band/repository/ArtistRepository.java
+++ src/main/java/ceos/diggindie/domain/band/repository/ArtistRepository.java
@@ -3,6 +3,9 @@ package ceos.diggindie.domain.band.repository;
 import ceos.diggindie.domain.band.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
+    List<Artist> findByBandId(Long bandId);
 }
 
