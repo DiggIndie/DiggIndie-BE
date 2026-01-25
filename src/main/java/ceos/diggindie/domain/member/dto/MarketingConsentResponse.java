3455553da5ae49b87574927ced11ac15f4952358
Hoyoung927 <hybyun0207@gmail.com>
2026-01-25 20:30:42 +0900

    fix : dto 수정

--- src/main/java/ceos/diggindie/domain/member/dto/MarketingConsentResponse.java
+++ src/main/java/ceos/diggindie/domain/member/dto/MarketingConsentResponse.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "마케팅 동의 응답")
 public record MarketingConsentResponse(
         @Schema(description = "마케팅 동의 여부", example = "true")
-        boolean marketingConsent
+        Boolean marketingConsent
 ) {
 }
 
